9471a2c55d7405e624322d86fc972dfc58c3e4d2
박찬규 <changyu.park1@gmail.com>
2021-08-11 20:12:32 +0900

    chapter1 수익률 대 수익률

---
+++ chapter1/4_수익률_대_수익률.py
@@ -0,0 +1,21 @@
+# 기간별 수익률을 returns 리스트에 저장한다. 
+returns = [0.1, 0.06, 0.05]
+sumOfReturn = 0.0
+arimean = 0.0
+geomean = 1.0
+n = len(returns)
+
+cnt = 0
+for r in returns:
+    sumOfReturn = sumOfReturn + r   
+    cnt += 1
+arimean = sumOfReturn / cnt
+print('AriMean is {:.2%}'.format(arimean))
+
+
+######################
+for r in returns:
+    geomean = geomean*(1+r)
+
+geomean = geomean**(1/cnt) -1
+print('geoMean is {:.2%}'.format(geomean))
